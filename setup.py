9ca3c8a1cf08a2f38c2429b18ebdece0c44e29c5
jr-42 <jjreed090@gmail.com>
2019-06-07 18:21:44 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+
+with open('requirements/base.pip') as f:
+    BASE_REQS = f.read().splitlines()
+
+with open('requirements/setup.pip') as f:
+    SETUP_REQS = f.read().splitlines()
+
+with open('requirements/test.pip') as f:
+    TEST_REQS = f.read().splitlines()
+
+with open('requirements/dev.pip') as f:
+    DEV_REQS = f.read().splitlines()
+
+with open('requirements/docs.pip') as f:
+    DOCS_REQS = f.read().splitlines()
+
+with open("README.md", "r") as freadme:
+    long_description = freadme.read()
+
+setup(
+    name='footbal_simulation',
+    author='J. J. Reed',
+    url='https://github.com/jr-42/football_simulation',
+    use_scm_version=True,
+    description='A football simulation',
+    long_description=long_description,
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    package_data={'': ['*.json']},
+    include_package_data=True,
+    install_requires=BASE_REQS,
+    setup_requires=SETUP_REQS,
+    extras_require={
+        'test': TEST_REQS,
+        'develop': TEST_REQS + DEV_REQS,
+        'docs':  DOCS_REQS
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.6'
+    ]
+)
---
+++ src/football/player.py
@@ -0,0 +1,83 @@
+import random
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+class Player:
+
+    def __init__(self):
+        self.__name = 'Joe'
+        self.__surname = 'Reed'
+        self.__dob = int((datetime(datetime.now().year-30, 1, 1) + \
+            int(365*random.uniform(-3.0, 10.0)) * timedelta(days=1)).strftime('%Y%m%d'))
+        self.__height = round(random.uniform(1.7, 1.9), 2)
+        self.__weight = random.randint(65, 95)
+        self.__foot = random.choice(['r', 'l'])
+        self.__ca = random.randint(20, 95)
+        self.__fa = self.__ca + random.randint(-10, 100-self.__ca)
+        # self.__value = (self.__ca/100 + self.__fa/100 + (33-self.age)/100.0) * 1000
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def surname(self):
+        return self.__surname
+    
+    @property
+    def dob(self):
+        return self.__dob
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    @property
+    def weight(self):
+        return self.__weight
+    
+    @property
+    def foot(self):
+        return self.__foot
+    
+    @property
+    def value(self):
+        return self.__value
+    
+    # @property
+    # def age(self):
+    #     dob = datetime.strptime(str(self.__dob), '%Y%m%d')
+    #     dtyears = dob - datetime.now()
+    #     age = dtyears.to_sec
+
+
+class Defender(Player):
+
+    def __init__(self):
+        Player.__init__(self)
+        self.__position = 'def'
+    
+    @property
+    def position(self):
+        return self.__position
+
+class Midfield(Player):
+
+    def __init__(self):
+        Player.__init__(self)
+        self.__position = 'mid'
+    
+    @property
+    def position(self):
+        return self.__position
+
+class Attacker(Player):
+
+    def __init__(self):
+        Player.__init__(self)
+        self.__position = 'att'
+    
+    @property
+    def position(self):
+        return self.__position
