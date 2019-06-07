e19e7f45bb451c003656eff183f73366481b5026
jr-42 <jjreed090@gmail.com>
2019-06-07 22:13:02 +0200

    Added team objects. Can now create a team which fills itself with players

--- src/football/players.py
+++ src/football/players.py
@@ -2,23 +2,35 @@ import random
 import pandas as pd
 from datetime import datetime, timedelta
 
-names = pd.read_csv('../../data/names.csv',
-                    header=None, sep=',')
+names = list(pd.read_csv('data/names.csv', index_col=0,
+                         header=None, sep=',').loc[:, 1].values)
+
+nationality = ['English', 'French', 'German', 'Spanish', 'Italian', 'Dutch',
+               'Brazilian', 'Argentinian', 'Scottish', 'Portuguese']
 
 
 class Player:
 
-    def __init__(self):
+    def __init__(self,
+                 team='Free Agent',
+                 team_rating=None,
+                 ):
         self.__name = random.choice(names)
         self.__surname = random.choice(names)
         self.__dob = int((datetime(datetime.now().year-30, 1, 1) +
                          int(365*random.uniform(-3.0, 10.0)
                              ) * timedelta(days=1)
                           ).strftime('%Y%m%d'))
+        self.__nationality = random.choice(nationality)
         self.__height = round(random.uniform(1.7, 1.9), 2)
         self.__weight = random.randint(65, 95)
-        self.__foot = random.choice(['r', 'l'])
-        self.__ca = random.randint(20, 95)
+        self.__foot = random.choice(['right', 'left'])
+        self.__team = team
+
+        if team_rating:
+            self.__ca = random.randint(20+((5-team_rating)*10), 95)
+        else:
+            self.__ca = random.randint(20, 95)
         self.__fa = self.__ca + random.randint(-10, 100-self.__ca)
         self.__value = (self.__ca/100 + self.__fa/100 + (33-self.age)/100.0
                         ) * 1000000
@@ -36,6 +48,10 @@ class Player:
         return self.__dob
 
     @property
+    def nationality(self):
+        return self.__nationality
+
+    @property
     def height(self):
         return self.__height
 
@@ -46,6 +62,10 @@ class Player:
     @property
     def foot(self):
         return self.__foot
+
+    @property
+    def team(self):
+        return self.__team
 
     @property
     def value(self):
@@ -59,10 +79,22 @@ class Player:
         return int(age)
 
 
+class Goalkeeper(Player):
+
+    def __init__(self, team=None, team_rating=None):
+        Player.__init__(self, team=None, team_rating=None)
+        self.__position = 'gk'
+        self.__value = self.value + 1000000
+
+    @property
+    def position(self):
+        return self.__position
+
+
 class Defender(Player):
 
-    def __init__(self):
-        Player.__init__(self)
+    def __init__(self, team=None, team_rating=None):
+        Player.__init__(self, team=None, team_rating=None)
         self.__position = 'def'
         self.__value = self.value + 3000000
 
@@ -73,8 +105,8 @@ class Defender(Player):
 
 class Midfielder(Player):
 
-    def __init__(self):
-        Player.__init__(self)
+    def __init__(self, team=None, team_rating=None):
+        Player.__init__(self, team=None, team_rating=None)
         self.__position = 'mid'
         self.__value = self.value + 7000000
 
@@ -85,8 +117,8 @@ class Midfielder(Player):
 
 class Attacker(Player):
 
-    def __init__(self):
-        Player.__init__(self)
+    def __init__(self, team=None, team_rating=None):
+        Player.__init__(self, team=None, team_rating=None)
         self.__position = 'att'
         self.__value = self.value + 11000000
 
---
+++ src/football/teams.py
@@ -0,0 +1,72 @@
+import random
+import pandas as pd
+from football.players import Goalkeeper, Defender, Midfielder, Attacker
+
+names = list(pd.read_csv('data/towns.csv', index_col=0,
+                         header=None, sep=',').loc[:, 1].values)
+team_suffix = ['Town', 'Rovers', 'United', 'City', '', 'Wanderers',
+               'F.C.', '']
+
+
+def team_dataframe(gks, defs, mids, atts):
+
+    players = gks + defs + mids + atts
+    cols = ['Name', 'DOB', 'Age', 'Nationality', 'Position', 'Foot', 'Value']
+    df = pd.DataFrame(columns=cols)
+
+    for i, player in enumerate(players):
+        df.loc[i, 'Name'] = player.name
+        df.loc[i, 'DOB'] = player.dob
+        df.loc[i, 'Age'] = player.age
+        df.loc[i, 'Nationality'] = player.nationality
+        df.loc[i, 'Position'] = player.position
+        df.loc[i, 'Foot'] = player.foot
+        df.loc[i, 'Value'] = player.value
+
+    return df
+
+
+class Team:
+
+    def __init__(self):
+        self.__name = random.choice(names) + random.choice(team_suffix)
+        self.__rating = random.randint(1, 5)
+        self.__gks = [Goalkeeper(team=self.__name, team_rating=self.__rating)
+                      for i in range(3)]
+        self.__defs = [Defender(team=self.__name, team_rating=self.__rating)
+                       for i in range(8)]
+        self.__mids = [Midfielder(team=self.__name, team_rating=self.__rating)
+                       for i in range(7)]
+        self.__atts = [Attacker(team=self.__name, team_rating=self.__rating)
+                       for i in range(5)]
+
+        self.__equip = team_dataframe(self.__gks, self.__defs,
+                                      self.__mids, self.__atts)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @property
+    def gks(self):
+        return self.__gks
+
+    @property
+    def defs(self):
+        return self.__defs
+
+    @property
+    def mids(self):
+        return self.__mids
+
+    @property
+    def atts(self):
+        return self.__atts
+    
+    @property
+    def theteam(self):
+        return self.__equip
