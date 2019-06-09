3be8d2bcd6948d2cbb80e99507e37bf087757e52
jr-42 <jjreed090@gmail.com>
2019-06-09 20:52:03 +0200

    Match class now plays a match with winner based on team rating/home advantage and player rating

--- src/football/match.py
+++ src/football/match.py
@@ -11,42 +11,62 @@ class Match:
         self.__home = team1
         self.__away = team2
 
-        @property
-        def home(self):
-            return self.__home
+    @property
+    def home(self):
+        return self.__home
 
-        @property
-        def away(self):
-            return self.__away
+    @property
+    def away(self):
+        return self.__away
 
-        @staticmethod
-        def result_probability(team1, team2):
-            pass
-            # return (p1, p2, pd)
+    def result_probability(self, team1, team2):
+        # relative player rating
+        team1_r = sum([i.rating for i in team1])/len(team1)/100.0
+        team2_r = sum([i.rating for i in team2])/len(team2)/100.0
+        
+        team1_rn = random.uniform(0.5, 0.6)*team1_r/(team1_r+team2_r)
+        team2_rn = random.uniform(0.5, 0.6)*team2_r/(team1_r+team2_r)
 
-        def play(self):
+        # home advantage
+        team1_r = team1_rn + random.uniform(0.1, 0.2)
+        team2_r = team2_rn
 
-            home11 = self.home.pick_team()
-            away11 = self.away.pick_team()
+        # relative team rating
+        team1_r = team1_r + random.uniform(0.15, 0.2)*self.home.rating / \
+            (self.home.rating+self.away.rating)
+        team2_r = team2_r + random.uniform(0.15, 0.2)*self.away.rating / \
+            (self.home.rating+self.away.rating)
 
-            h, a, d = self.result_probability(home11, away11)
+        # Final 
+        p1 = team1_r
+        p2 = team2_r
+        pdr = 1 - p1 - p2
 
-            v = random.random()
+        return (p1, p2, pdr)
 
-            if v <= h:
-                hp = 3
-                ap = 0
-                score = random.choice([(2, 1), (3, 1), (1, 0), (2, 0)])
-            elif h < v <= (h+a):
-                hp = 0
-                ap = 3
-                score = random.choice([(0, 1), (1, 3), (1, 2), (0, 2)])
-            else:
-                hp = 1
-                ap = 1
-                score = random.choice([(1, 1), (3, 3), (2, 2), (0, 0)])
+    def play(self):
 
-            return hp, ap, score
+        home11 = self.home.pick_team()
+        away11 = self.away.pick_team()
+
+        h, a, d = self.result_probability(home11, away11)
+
+        v = random.random()
+
+        if v <= h:
+            hp = 3
+            ap = 0
+            score = random.choice([(2, 1), (3, 1), (1, 0), (2, 0)])
+        elif h < v <= (h+a):
+            hp = 0
+            ap = 3
+            score = random.choice([(0, 1), (1, 3), (1, 2), (0, 2)])
+        else:
+            hp = 1
+            ap = 1
+            score = random.choice([(1, 1), (3, 3), (2, 2), (0, 0)])
+
+        return hp, ap, score
 
 
 
---
+++ src/football/worker.py
@@ -0,0 +1,7 @@
+import football.teams as ft
+import football.match as fm
+
+
+def test_match():
+    m = fm.Match(ft.Team(), ft.Team())
+    return m
